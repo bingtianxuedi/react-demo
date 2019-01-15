0ec0b3db5295d3792ae396a91bac8f0bf27d598f
Your Name <you@example.com>
2019-01-15 15:27:01 +0800

    第三章节

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import {PropTypes} from  'prop-types';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Greeter extends Component{
+    render() {
+        return(
+            <h1>
+                {this.props.salutation}
+            </h1>
+        )
+    }
+}
+Greeter.propTypes = {
+    salutation: PropTypes.string.isRequired
+}
+Greeter.defaultProps = {
+    salutation:"Hello World"
+}
+
+
+
+ReactDOM.render(<Greeter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+
+
+
+
+
+
+
